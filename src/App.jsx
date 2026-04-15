a89d1246d61b3d09a1e0874a7a79ce3dd765690c
Ander <115133224+ander-ryu@users.noreply.github.com>
2026-04-16 00:07:09 +0900

    学了react的基本语法 和按钮控制弹窗和文字显示或者隐藏

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 function FeatureList() {
   return (
     <div>
@@ -12,9 +14,52 @@ function FeatureList() {
 }
 
 function App() {
+  const name = "LIU"
+  const [showText, setShouText] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const handleClick = () =>{
+    alert("点我干鸡毛！😒")
+  }
   return (
-    <div>
-      <h1>Engineer Matching System</h1>
+    <div style={{textAlign:"center",marginTop:"50px"}}>
+      <h1>我的第一个 React 项目</h1>
+      <h1>Hello,{name}</h1>
+      <h1>点击次数：{count}</h1>
+      <button 
+      onClick={()=> setCount(count+1)}
+      style={{
+        fontSize:"16px",
+        padding:"10px 20px",
+        marginTop:"55px",
+        marginBottom:"55px",
+        marginRight:"20px",
+        borderRadius:"20px",
+        cursor:"pointer"
+      }}>点我一下试试</button>
+
+      <button
+      onClick={() => setCount(0)}
+      style={{
+        fontSize:"16px",
+        padding:"10px 20px",
+        marginTop:"55px",
+        marginBottom:"55px",
+        marginLeft:"20px",
+        borderRadius:"20px",
+        cursor:"pointer"
+      }}>次数清零</button>
+
+      <button
+      onClick={()=> setShouText(!showText)}
+      style={{
+          fontSize: "24px",
+          padding: "10px 20px",
+          cursor: "pointer",
+          borderRadius: "10px"
+        }}>显示/隐藏文字</button>
+
+        {showText && <p>这段文字出来了！</p>}
       <p>React learning starts here.</p>
       <FeatureList />
     </div>
